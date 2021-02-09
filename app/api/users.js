5e31eb86fe42d10d01fe46c1dbf9fbeccf39d22c
avinashvintek <avinash@vinteksolution.com>
2021-02-09 10:33:31 +0530

    Push notification of video call

--- app/api/users.js
+++ app/api/users.js
@@ -14,8 +14,18 @@ const getVideoToken = (username) => {
   return client.get(`${endPoint}/getToken/?userName=${username}`)
 }
 
+const createPushToken = (token) => {
+  return client.patch(`${endPoint}/saveToken`, token)
+}
+
+const sendPushNotification = (data) => {
+  return client.post(`${endPoint}/sendNotification`, data)
+}
+
 export default {
   getUsers,
   getLoggedInUser,
   getVideoToken,
+  createPushToken,
+  sendPushNotification,
 }
--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,5 +1,5 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { StyleSheet, View } from 'react-native'
+import React, { useContext, useEffect, useState, useRef } from 'react'
+import { StyleSheet, View, Platform, Alert } from 'react-native'
 
 import AppButton from '../components/AppButton'
 import AppText from '../components/AppText'
@@ -8,17 +8,153 @@ import authStorage from '../components/utils/authStorage'
 
 import LoadingIndicator from '../components/LoadingIndicator'
 import usersApi from '../api/users'
-
+import * as Notifications from 'expo-notifications'
+import * as Permissions from 'expo-permissions'
 import socket from '../components/utils/socket'
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: false,
+  }),
+})
 
 const HomeScrren = ({ navigation }) => {
   const { user, setUser } = useContext(AuthContext)
   const [loading, setLoading] = useState(false)
+  const notificationListener = useRef()
+  const responseListener = useRef()
+  const [dataSend, setDataSend] = useState(false)
+  // const [userToken, userToken.current] = useState(null)
+  const userToken = useRef(null)
 
   const handleLogout = () => {
     setUser()
     authStorage.removeToken()
   }
+
+  const sendPushToken = async (token, message, customData) => {
+    setLoading(true)
+
+    const pushRes = await usersApi.sendPushNotification({
+      targetExpoPushToken: token,
+      title: 'Doctor Response!',
+      message: message,
+      data: customData,
+    })
+    if (!pushRes.ok) {
+      setLoading(false)
+      console.log('Error', pushRes)
+      return
+    }
+    userToken.current = null
+    setDataSend(false)
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    const saveNotificationToken = async () => {
+      const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS)
+      if (status !== 'granted') {
+        alert('No notification permissions!')
+        return
+      }
+      try {
+        const token = await Notifications.getExpoPushTokenAsync({
+          experienceId: `@vetinstant/docvetInstant`,
+        })
+
+        // console.log(token.data)
+
+        if (user.token && user.token === token.data) {
+          return
+        }
+
+        const res = await usersApi.createPushToken({ token: token.data })
+        if (!res.ok) {
+          console.log('Error', res.data)
+          return
+        }
+
+        setUser(res.data.user)
+      } catch (error) {
+        console.log('Error getting push token', error)
+      }
+    }
+    saveNotificationToken()
+
+    if (Platform.OS === 'android') {
+      Notifications.setNotificationChannelAsync('default', {
+        name: 'default',
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+      })
+    }
+
+    notificationListener.current = Notifications.addNotificationReceivedListener(
+      (notification) => {
+        // console.log('REceived', notification)
+        if (!userToken.current && notification.request.content.data.token) {
+          console.log('Received', userToken.current)
+          userToken.current = notification.request.content.data.token
+
+          Alert.alert(
+            'Incoming Call Request from PetOwner',
+            `Are you available for next 15-30 minutes?\n** Don't close the app from background!!`,
+            [
+              {
+                text: 'Cancel',
+                onPress: () => {
+                  console.log('Cancel Pressed')
+                  if (!dataSend) {
+                    sendPushToken(
+                      userToken.current,
+                      `Sorry! I'm not available. Please try with other available doctors`,
+                      { status: 'cancel' }
+                    )
+                    console.log('Data Send')
+                    setDataSend(true)
+                  }
+                },
+                style: 'cancel',
+              },
+              {
+                text: 'Accept',
+                onPress: () => {
+                  if (!dataSend) {
+                    sendPushToken(
+                      userToken.current,
+                      `Yes I'm available. Complete the payment within 5-10 minutes`,
+                      { status: 'ok' }
+                    )
+                    console.log('Data Send')
+                    setDataSend(true)
+                  }
+                  console.log('OK Pressed')
+                  return
+                },
+              },
+            ],
+            { cancelable: false }
+          )
+        }
+      }
+    )
+
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(
+      (notification) => {
+        console.log(userToken.current)
+        console.log('Response', notification)
+      }
+    )
+
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener)
+      Notifications.removeNotificationSubscription(responseListener)
+      userToken.current = null
+    }
+  }, [])
 
   const handleVideoPress = async () => {
     const tokenRes = await usersApi.getVideoToken(user.name)
@@ -52,6 +188,44 @@ const HomeScrren = ({ navigation }) => {
         onPress={() => navigation.navigate('PatientList')}
       />
       <AppButton title='Video Call' onPress={handleVideoPress} />
+      {/* {userToken.current &&
+        Alert.alert(
+          'Incoming Call Request from PetOwner',
+          `Are you available for next 15-30 minutes?\n** Don't close the app from background!!`,
+          [
+            {
+              text: 'Cancel',
+              onPress: () => {
+                console.log('Cancel Pressed')
+                if (!dataSend) {
+                  sendPushToken(
+                    userToken.current,
+                    `Sorry! I'm not available. Please try with other available doctors`
+                  )
+                  console.log('Data Send')
+                  setDataSend(true)
+                }
+              },
+              style: 'cancel',
+            },
+            {
+              text: 'Accept',
+              onPress: () => {
+                if (!dataSend) {
+                  sendPushToken(
+                    userToken.current,
+                    `Yes I'm available. Complete the payment within 5-10 minutes`
+                  )
+                  console.log('Data Send')
+                  setDataSend(true)
+                }
+                console.log('OK Pressed')
+                return
+              },
+            },
+          ],
+          { cancelable: false }
+        )} */}
     </View>
   )
 }
