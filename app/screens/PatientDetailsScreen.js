3bd124cfc90d310374581acb24ca54fb8e44b5f7
avinashvintek <avinash@vinteksolution.com>
2021-02-22 15:23:41 +0530

    PatientDetailsModified

--- app/screens/PatientDetailsScreen.js
+++ app/screens/PatientDetailsScreen.js
@@ -67,6 +67,7 @@ const PatientDetailsScreen = ({ navigation, route }) => {
               {pet.months !== 0 && `${pet.months} months`}
             </AppText>
             <AppText>Gender: {pet.gender} </AppText>
+            <AppText>Breed: {pet.breed} </AppText>
             <AppText>Pet Type: {pet.type} </AppText>
             {pet.petHistoryImages.length > 0 && (
               <>
@@ -75,8 +76,11 @@ const PatientDetailsScreen = ({ navigation, route }) => {
                   {pet.petHistoryImages.map((img, i) => (
                     <View key={`${img}-${i}`} style={{ marginRight: 20 }}>
                       <Image
+                        // source={{
+                        //   uri: `https://vetinstantbe.azurewebsites.net/${img}`,
+                        // }}
                         source={{
-                          uri: `https://vetinstantbe.azurewebsites.net/api/v1${img}`,
+                          uri: `http://192.168.43.242:8000/${img}`,
                         }}
                         style={{ width: 150, height: 150, borderRadius: 75 }}
                       />
@@ -85,6 +89,78 @@ const PatientDetailsScreen = ({ navigation, route }) => {
                 </ScrollView>
               </>
             )}
+            {pet.prescriptions.length > 0 && (
+              <AppText>Pet Prescriptions:</AppText>
+            )}
+            {pet.prescriptions.length > 0 &&
+              pet.prescriptions.map((pbm, index) => (
+                <View key={index} style={styles.cardBordered}>
+                  <AppText>Prescription: {pbm.prescription}</AppText>
+                  <AppText>Doctor name: {pbm.docname}</AppText>
+                  <AppText>
+                    Date: {new Date(pbm.date).toLocaleDateString()}
+                  </AppText>
+                  <AppText>
+                    Time: {new Date(pbm.date).toLocaleTimeString()}
+                  </AppText>
+                  {pbm.img && (
+                    <>
+                      <AppText>Prescription Image:</AppText>
+                      <Image
+                        source={{
+                          uri: `http://192.168.43.242:8000/${pbm.img}`,
+                        }}
+                        // source={{
+                        //   uri: `https://vetinstantbe.azurewebsites.net/api/v1/${pbm.img}`,
+                        // }}
+                        style={{ width: 150, height: 150, borderRadius: 75 }}
+                      />
+                    </>
+                  )}
+                </View>
+              ))}
+            {pet.problems.length > 0 && <AppText>Pet Problems:</AppText>}
+            {pet.problems.length > 0 &&
+              pet.problems.map((pb, index) => (
+                <View key={pb._id} style={styles.cardBordered}>
+                  <AppText>Problem: {pb.problem}</AppText>
+                  <AppText>Doctor name: {pb.docname}</AppText>
+                  <AppText>Time Period: {pb.time}</AppText>
+                  <AppText>Appetite: {pb.Appetite}</AppText>
+                  <AppText>Behaviour: {pb.Behaviour}</AppText>
+                  <AppText>Ears: {pb.Ears}</AppText>
+                  <AppText>Eyes: {pb.Eyes}</AppText>
+                  <AppText>Faces: {pb.Feces}</AppText>
+                  <AppText>Gait: {pb.Gait}</AppText>
+                  <AppText>Mucous: {pb.Mucous}</AppText>
+                  <AppText>Skin: {pb.Skin}</AppText>
+                  <AppText>Urine: {pb.Urine}</AppText>
+                  <AppText>Comment: {pb.comment}</AppText>
+                  {pb.images.length > 0 && <AppText>Pet Problem image</AppText>}
+                  <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+                    {pb.images.length > 0 &&
+                      pb.images.map((img, i) => (
+                        <>
+                          <Image
+                            key={i + img}
+                            source={{
+                              uri: `http://192.168.43.242:8000/${img}`,
+                            }}
+                            // source={{
+                            //   uri: `https://vetinstantbe.azurewebsites.net/api/v1/${img}`,
+                            // }}
+                            style={{
+                              width: 150,
+                              height: 150,
+                              borderRadius: 75,
+                              marginHorizontal: 5,
+                            }}
+                          />
+                        </>
+                      ))}
+                  </ScrollView>
+                </View>
+              ))}
           </View>
           <AppButton
             title='Chat'
@@ -102,13 +178,22 @@ const PatientDetailsScreen = ({ navigation, route }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingVertical: 30,
+    // paddingVertical: 30,
     marginHorizontal: 30,
+    marginVertical: 10,
   },
   card: {
     padding: 15,
     backgroundColor: '#fff',
     borderRadius: 10,
+  },
+  cardBordered: {
+    borderColor: '#dfd9d9',
+    borderWidth: 1,
+    paddingVertical: 10,
+    paddingHorizontal: 8,
+    marginVertical: 5,
+    borderRadius: 5,
   },
 })
 
