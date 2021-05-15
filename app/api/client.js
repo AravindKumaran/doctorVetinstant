4f089b4328be1a36a4c29f09e48d278eb579c090
Tamilvanan25 <tamilvanan@vinteksolution.com>
2021-05-15 15:18:42 +0530

    New Backend URL

--- app/api/client.js
+++ app/api/client.js
@@ -1,17 +1,19 @@
-import { create } from 'apisauce'
-import authStorage from '../components/utils/authStorage'
+import { create } from "apisauce";
+import authStorage from "../components/utils/authStorage";
 
 const apiClient = create({
   // baseURL: "http://localhost:8000/api/v1",
   // baseURL: 'https://vetinstant.azurewebsites.net/api/v1',
-  baseURL: 'https://vetinstantbe.azurewebsites.net/api/v1',
+  // baseURL: 'https://vetinstantbe.azurewebsites.net/api/v1',
   // baseURL: 'http://192.168.43.242:8000/api/v1',
-})
+  baseURL:
+    "http://ec2-13-126-97-116.ap-south-1.compute.amazonaws.com:8000/api/v1",
+});
 
 apiClient.addAsyncRequestTransform(async (request) => {
-  const authToken = await authStorage.getToken()
-  if (!authToken) return
-  request.headers['Authorization'] = `Bearer ${authToken}`
-})
+  const authToken = await authStorage.getToken();
+  if (!authToken) return;
+  request.headers["Authorization"] = `Bearer ${authToken}`;
+});
 
-export default apiClient
+export default apiClient;
